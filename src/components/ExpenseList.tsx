eb32992420770efc70494351d58675eae825cd73
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-01-03 23:59:14 +0100

    Style app

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -13,31 +13,35 @@ const ExpenseList = ({ expenses, onDelete }: Props) => {
   if (expenses.length === 0) return null;
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Description</th>
-          <th>Amount</th>
-          <th>Category</th>
-          <th></th>
+    <table className='border-2 mx-auto'>
+      <thead className=''>
+        <tr className=''>
+          <th className='border-2 p-2.5'>Description</th>
+          <th className='border-2 p-2.5'>Amount</th>
+          <th className='border-2 p-2.5'>Category</th>
+          <th className='border-2 p-2.5'></th>
         </tr>
       </thead>
       <tbody>
         {expenses.map((expense) => (
-          <tr key={expense.id}>
-            <td>{expense.description}</td>
-            <td>{expense.amount}</td>
-            <td>{expense.category}</td>
-            <td>
-              <button onClick={() => onDelete(expense.id)}>Delete</button>
+          <tr key={expense.id} className='text-center'>
+            <td className='border-2 p-2.5 '>{expense.description}</td>
+            <td className='border-2 p-2.5'>{expense.amount}</td>
+            <td className='border-2 p-2.5'>{expense.category}</td>
+            <td className='border-2 p-2.5'>
+              <button
+                className='bg-purple-700 py-1 px-2 border-2 border-transparent rounded-lg hover:border-2 hover:border-orange-600'
+                onClick={() => onDelete(expense.id)}>
+                Delete
+              </button>
             </td>
           </tr>
         ))}
       </tbody>
       <tfoot>
-        <tr>
-          <td>Total</td>
-          <td>
+        <tr className='text-center font-bold'>
+          <td className='border-2 p-2.5'>Total</td>
+          <td className='border-2 p-2.5'>
             $
             {expenses
               .reduce((acc, expense) => expense.amount + acc, 0)
